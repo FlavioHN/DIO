623606227ce010366f18588c5f4a0154ae745636
flaviopinheiro <flavio.aphn@gmail.com>
2022-08-08 23:18:58 -0300

    Adicionado arquivo de criacao de usuarios

---
+++ linux_experience/createuser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Necessario que os grupos estejam criados para funcionar corretamente
+
+echo "Criando usuarios"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+
+echo "Usuarios criados e adicionados aos seus grupos correspondentes..."
