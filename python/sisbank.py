295dd1bbcb96d8ffb66ba60f3c64b5618a44e994
Flavio Pinheiro <37315226+FlavioHN@users.noreply.github.com>
2023-07-05 01:04:19 -0300

    adicionando estruturas de decisoes

--- python/sisbank.py
+++ python/sisbank.py
@@ -1,13 +1,22 @@
-menu = ('''
-    Bem Vindo ao SisBank
+from time import sleep
 
+menu = ('''
+|============##$##============|
+|   SisBank Crypto Finance    |
+|============##$##============|
+    
+    |------ Serviços ------|
+    
         [1] - Saldo
         [2] - Deposito
         [3] - Saque
         [0] - Finalizar
-    ---------------------
+    
+    |----------------------|
 ''')
 print(menu)
+sleep(2)
+
 saldo = 0
 deposito = 0
 retirada = 0
@@ -17,14 +26,20 @@ encerrar = ("Obrigado por utilizar nossos serviços!...")
 #while servico != 0:
 while True:
 
+    print("=", "-"*30,"=")
     servico = int(input('Selecione a operação desejada: '))
+    print("=", "-"*30,"=\n")
 
-    if servico == 1:
+    if servico > 3:
+        print("Opção invalida, favor digitar um serviço listado.")
+        print(menu)
+    
+    elif servico == 1:
         print(f"Saldo disponivel R$ {saldo:.2f}")
         retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
-        if retorno_menu == 'sim':
+        if retorno_menu.lower() == 'sim':
             print(menu)
-        elif retorno_menu == 'nao':
+        elif retorno_menu.lower() == 'nao':
             print(encerrar)
             break
         
@@ -38,49 +53,55 @@ while True:
             print("Desposito realizado com sucesso!")
             
             retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
-            if retorno_menu == 'sim':
+            if retorno_menu.lower() == 'sim':
                 print(menu)
-            elif retorno_menu == 'nao':
+            elif retorno_menu.lower() == 'nao':
                 print(encerrar)
                 break
         
         elif confirmar_deposito == 'nao':
-            print(menu)
+            print("deposito não realizado!.")
+            retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
+            if retorno_menu.lower() == 'sim':
+                print(menu)
+            elif retorno_menu.lower() == 'nao':
+                print(encerrar)
+                break
+            
     elif servico == 3:
         saque = int(input("Digite o valor a ser retirado: "))
-        if saque <= saldo and retirada <= 4:
+        if saque <= saldo and retirada < 3:
             saldo -= saque
             retirada +=1
             print(f"Saque no valor R$ {saque:.2f} realizado com sucesso!")
             
             retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
-            if retorno_menu == 'sim':
+            if retorno_menu.lower() == 'sim':
                 print(menu)
-            elif retorno_menu == 'nao':
+            elif retorno_menu.lower() == 'nao':
                 print(encerrar)
                 break
 
-        elif saque <= saldo and retirada == 4:
+        elif saque <= saldo and retirada == 3:
             print("Quantidade de saques diario excedido! \nRetorne amanha para realizar um novo saque!")
             
             retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
-            if retorno_menu == 'sim':
+            if retorno_menu.lower() == 'sim':
                 print(menu)
-            elif retorno_menu == 'nao':
+            elif retorno_menu.lower() == 'nao':
                 print(encerrar)
                 break
-
 
         elif saque > saldo:
             print("Saldo em conta menor que o solicitado!")
             
             retorno_menu = str(input("Realizar nova operação: - [Sim] - [Nao]: "))
-            if retorno_menu == 'sim':
+            if retorno_menu.lower() == 'sim':
                 print(menu)
-            elif retorno_menu == 'nao':
+            elif retorno_menu.lower() == 'nao':
                 print(encerrar)
                 break
-
-#else:
-#print(encerrar)
-
+            
+    elif servico == 0:
+        print(encerrar)
+        break
